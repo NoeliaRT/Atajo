8a6d2fc22ec094ef7af21543fef2273f9788bb8e
Noelia <Cristian@DESKTOP-9RQ4IO4>
2022-02-19 12:31:16 +0100

    Creación método longitudDiferente

--- RutaMasCorta/src/gps/Bicicleta.java
+++ RutaMasCorta/src/gps/Bicicleta.java
@@ -13,25 +13,18 @@ public class Bicicleta {
         String marca = "Orbea";
     }
     
-    public static String rutaCorta(int [] camino1, int [] camino2, int [] camino3)
-    {
-        int sum1 = 0;
-        int sum2 = 0;
-        int sum3 = 0;
-        
-        for(int i=0; i<camino1.length; i++)
-        {
-            sum1 += camino1[i];           
-        }
-        for(int i=0; i<camino2.length; i++)
-        {
-            sum2 += camino2[i];           
-        }
-        for(int i=0; i<camino3.length; i++)
-        {
-            sum3 += camino3[i];           
-        }
-        
+    public static int longituDiferente(int [] camino) {
+    	
+    	int suma=0;
+    	for(int i=0; i<camino.length; i++) {
+    		suma += camino[i];
+    	}
+    	return suma;
+    }
+    
+    public static String rutaCorta(int sum1, int sum2, int sum3)
+    {      	
+           
         if(sum1 < sum2 && sum1 < sum3)
         {
             return "camino 1"; 
@@ -59,10 +52,16 @@ public class Bicicleta {
 
     public static void main(String[] args) {
         Bicicleta bici = new Bicicleta();
+        
         int[] camino1 = {12,34,21,46,25};
         int[] camino2 = {24,1,5,64,10,15,21};
         int[] camino3 = {1,5,6,7,8,10,20,46,104};
-        System.out.println("El camino más corto es:"+Bicicleta.rutaCorta(camino1,camino2,camino3)+".");
+        
+        int sum1 = Bicicleta.longituDiferente(camino1);
+        int sum2 = Bicicleta.longituDiferente(camino2);
+        int sum3 = Bicicleta.longituDiferente(camino3);
+        
+        System.out.println("El camino más corto es:"+Bicicleta.rutaCorta(sum1,sum2,sum3)+".");
     }
 }
 
